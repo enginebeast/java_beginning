6896691866dfa6908487d836a6a6013bb7e62ce9
Engine Beast <131536099+enginebeast@users.noreply.github.com>
2025-09-11 17:13:33 +0900

    data type

---
+++ Datatype.java
@@ -0,0 +1,18 @@
+package hello;
+
+public class Datatype {
+
+	public static void main(String[] args) {
+		char a = 'A';
+		int b = 100;
+		long c = 100000l;
+		float d = 1.23456789f;
+		
+		System.out.println("a = " + a);
+		System.out.println("int a = " + (int)a);
+		System.out.println("b = " + b);
+		System.out.println("c = " + c);
+		System.out.println("d = " + d);
+	}
+
+}
